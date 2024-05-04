dcdc1aba6bb81882bb7a43203f2bc86727fb935d
fuminopen <fuminopen@gmail.com>
2024-05-04 21:16:05 +0900

    Exercise系のモデルとファクトリ追加

--- database/migrations/2024_05_04_01_014005_create_exercises_table.php
+++ database/migrations/2024_05_04_01_014005_create_exercises_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('excercices', function (Blueprint $table) {
+        Schema::create('exercices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
