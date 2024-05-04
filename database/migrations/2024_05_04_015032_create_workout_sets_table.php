830f6b8dc9939433dce4050db4bcd87954137891
fuminopen <fuminopen@gmail.com>
2024-05-04 19:58:50 +0900

    workout_setsテーブルの作成

---
+++ database/migrations/2024_05_04_015032_create_workout_sets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('workout_sets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('workout_detail_id')->constrained('workout_details');
+            $table->integer('set_number');
+            $table->integer('reps');
+            $table->float('weight');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('workout_sets');
+    }
+};
