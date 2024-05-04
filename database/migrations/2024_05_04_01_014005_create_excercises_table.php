042fa70c7565110f4c39b93dfe1bcb460382f25c
fuminopen <fuminopen@gmail.com>
2024-05-04 10:43:54 +0900

    タイポの修正

--- database/migrations/2024_05_04_01_014005_create_excercises_table.php
+++ database/migrations/2024_05_04_01_014005_create_excercises_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('excersices', function (Blueprint $table) {
+        Schema::create('excercices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('excersices');
+        Schema::dropIfExists('excercices');
     }
 };
