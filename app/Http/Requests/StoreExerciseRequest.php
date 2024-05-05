2ead4f9e4a69a322ad1d8bd799950b90f0719869
fuminopen <fuminopen@gmail.com>
2024-05-05 22:30:16 +0900

    Requestクラスにルールを追加

--- app/Http/Requests/StoreExerciseRequest.php
+++ app/Http/Requests/StoreExerciseRequest.php
@@ -22,7 +22,9 @@ class StoreExerciseRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'body_part_id' => 'required|integer|exists:body_parts,id'
         ];
     }
 }
