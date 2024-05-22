5c2f4d3c49b9a3082b30646c5b1a34beb84e322b
fuminopen <fuminopen@gmail.com>
2024-05-22 21:04:50 +0900

    BodyPart | StoreTestの修正

--- tests/Feature/BodyPart/StoreTest.php
+++ tests/Feature/BodyPart/StoreTest.php
@@ -3,18 +3,27 @@
 namespace Tests\Feature\BodyPart;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StoreTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
     public function test_can_create_body_part()
     {
         $response = $this->postJson(route('body_parts.store'), ['name' => 'Arms']);
+
         $response->assertCreated();
-        $response->assertJsonStructure(['id', 'name', 'created_at', 'updated_at']);
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
     }
 }
