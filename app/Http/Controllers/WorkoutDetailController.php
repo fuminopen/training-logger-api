aee5ec5112961fee7f8d16c379e66ae710d24b0f
fuminopen <fuminopen@gmail.com>
2024-06-14 21:35:02 +0900

    WorkoutDetail | エンドポイントの作成

--- app/Http/Controllers/WorkoutDetailController.php
+++ app/Http/Controllers/WorkoutDetailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WorkoutDetail\StoreWorkoutDetailRequest;
 use App\Http\Resources\WorkoutDetailResource;
 use App\Models\WorkoutDetail;
 use Illuminate\Http\Request;
@@ -17,19 +18,12 @@ class WorkoutDetailController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreWorkoutDetailRequest $request)
     {
-        //
+        $workoutDetail = WorkoutDetail::create($request->validated());
+        return new WorkoutDetailResource($workoutDetail);
     }
 
     /**
